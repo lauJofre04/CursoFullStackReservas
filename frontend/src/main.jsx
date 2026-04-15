701d8b01839ae6df46fadcf5f65edbd31e86f4ea
Lautaro Jofre <lautaro.jofre2004@gmail.com>
2026-04-15 11:51:42 -0300

    googleAuth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { GoogleAuthProvider } from '@react-oauth/google'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* Envolvemos toda la aplicación con el proveedor de Google */}
-    <GoogleAuthProvider clientId={clientId}>
+    <GoogleOAuthProvider clientId={clientId}>
       <App />
-    </GoogleAuthProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
